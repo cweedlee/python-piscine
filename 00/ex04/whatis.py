c371b244f772a44d9bf3a8fb4369a1ec820a7638
cweedlee <siwolee@student.42seoul.kr>
2025-07-23 15:08:13 +0900

    feat: ex04

---
+++ 00/ex04/whatis.py
@@ -0,0 +1,27 @@
+import sys
+
+def main() -> int : 
+    ### error case
+    # check the argv length
+    if len(sys.argv) > 2 :
+        print("AssertionError: more than one argument is provided") ; return 1 ; 
+    elif len(sys.argv) == 1 : 
+        return 1 ; 
+
+    val = 0;
+    # check the argv is integer
+    try:  
+        val = int(sys.argv[1])
+    except ValueError as e: 
+        print("AssertionError: argument is not an integer") ; return 1 ; 
+
+    ### check
+    # check if it's odd or even
+    if (val % 2) :
+        print("I'm Odd.");
+    else :
+        print("I'm Even.");
+    return 0;
+
+
+main()
